3e2cff565e1db046dcf7c297d2e8806f23c50b79
Kevin Trullemans <trullemans.k@gmail.com>
2019-06-26 16:20:43 +0200

    Eloquent - User + Profile

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,15 +7,15 @@
             <img src="https://instagram.fbru2-1.fna.fbcdn.net/vp/b0c5bb9cdb6cec7323f93c720f746f2d/5D8CE438/t51.2885-19/s150x150/22709172_932712323559405_7810049005848625152_n.jpg?_nc_ht=instagram.fbru2-1.fna.fbcdn.net" class="rounded-circle">
         </div>
         <div class="col-9 p-5">
-            <div><h1>freecodecamp</h1></div>
+            <div><h1>{{$user->username}}</h1></div>
             <div class="d-flex">
                 <div class="pr-5"><strong>153</strong> posts</div>
                 <div class="pr-5"><strong>23k</strong> followers</div>
                 <div class="pr-5"><strong>212</strong> following</div>
             </div>
-            <div class="pt-4 font-weight-bold">freeCodeCamp.org</div>
-            <div>We're a global community of millions of people learning to code together. We're an open source, donor-supported, 501(c)(3) nonprofit.</div>
-            <div><a href="#">www.freecodecamp.rog</a></div>
+            <div class="pt-4 font-weight-bold">{{$user->profile->title}}</div>
+            <div>{{$user->profile->description}}</div>
+            <div><a href="{{$user->profile->url}}" target="_blank">{{$user->profile->url}}</a></div>
         </div>
     </div>
     <div class="row pt-4">
