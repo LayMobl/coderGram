49a1c58ed52e6b45a62c93668c4e9d52efdc66e8
Kevin Trullemans <trullemans.k@gmail.com>
2019-07-08 13:06:01 +0200

    Policies + Auto Profile creation

--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -4,13 +4,18 @@
 <div class="container">
     <div class="row">
         <div class="col-3 p-5">
-            <img src="https://instagram.fbru2-1.fna.fbcdn.net/vp/b0c5bb9cdb6cec7323f93c720f746f2d/5D8CE438/t51.2885-19/s150x150/22709172_932712323559405_7810049005848625152_n.jpg?_nc_ht=instagram.fbru2-1.fna.fbcdn.net" class="rounded-circle">
+            <img src="{{asset('/storage/'.$user->profile->image)}}" class="rounded-circle w-100">
         </div>
         <div class="col-9 p-5">
             <div class="d-flex justify-content-between align-items-baseline">
                 <h1>{{$user->username}}</h1>
-                <a href="{{url('p/create')}}">Add new post</a>
+                @can('update', $user->profile)
+                    <a href="{{url('p/create')}}">Add new post</a>
+                @endcan
             </div>
+            @can('update', $user->profile)
+                <a href="{{url('profile/'.$user->id.'/edit')}}">Edit profile</a>
+            @endcan
             <div class="d-flex">
                 <div class="pr-5"><strong>{{count($user->posts)}}</strong> posts</div>
                 <div class="pr-5"><strong>23k</strong> followers</div>
